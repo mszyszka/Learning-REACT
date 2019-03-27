da995f73e895bdd914576209585739ac96e3d968
Michal <mszyszka223@gmail.com>
2019-03-27 13:04:51 +0100

    I wrote simple app with react

--- Lekcja5 - podział na komponenty/script.js
+++ Lekcja5 - podział na komponenty/script.js
@@ -1,14 +1,19 @@
 function App() {
   return (
     <div>
-
+      <AppHeader />
+      <main className="ui main text container">
+        <ContactList />
+      </main>
     </div>
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("app"));
+
 function AppHeader() {
   return (
-    <header classNameName="ui fixed menu">
+    <header className="ui  menu">
       <nav className="ui container">
         <a href="#" className="header item">
           <img className="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
@@ -24,21 +29,34 @@ function AppHeader() {
 
 function ContactList() {
   return (
-    <main className="ui main text container">
       <ul className="ui relaxed divided list selection">
-
+        <ContactItem 
+                login="typeofweb2"
+                name="Lena"
+                department="JavaScript Developer"
+        />
+        <ContactItem 
+                login="typeofweb1"
+                name="Mark"
+                department="Java Master"
+        />
+        <ContactItem 
+                login="typeofweb3"
+                name="John"
+                department="Head Hunterd"
+        />
       </ul>
-   </main>
   );
 }
 
-function ContactItem() {
+function ContactItem({login, name, department}) {
+  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
   return (
     <li className="item">
-      <img src="https://api.adorable.io/avatars/55/typeofweb1.png" className="ui mini rounded image" />
+      <img src= {imgUrl} className="ui mini rounded image" />
       <div className="content">
-        <h4 className="header">Lena</h4>
-        <div className="description">JavaScript Developer</div>
+        <h4 className="header">{name}</h4>
+        <div className="description">{department}</div>
       </div>
     </li>
     );  
