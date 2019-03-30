4231687768f94696405b1d0b1822468fd4b62e36
Michal <mszyszka223@gmail.com>
2019-03-30 11:06:58 +0100

    I rewrite components to classes

--- Lekcja5 - podział na komponenty/script.js
+++ Lekcja5 - podział na komponenty/script.js
@@ -1,3 +1,4 @@
+import React from 'react';
 /////////////////////Lekcja 5 ///////////////////////////
 function App() {
   return (
@@ -10,10 +11,68 @@ function App() {
   );
 }
 
+
 ReactDOM.render(<App />, document.getElementById("app"));
 
-function AppHeader() {
-  return (
+// function AppHeader() {
+//   return (
+//     <header className="ui  menu">
+//       <nav className="ui container">
+//         <a href="#" className="header item">
+//           <img className="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
+//           Lista kontaktów
+//         </a>
+//         <div className="header item">
+//           <button className="ui button">Dodaj</button>
+//         </div>
+//       </nav>
+//     </header>
+//     );
+// }
+
+
+
+// function ContactList() {
+//   return (
+//       <ul className="ui relaxed divided list selection">
+//         <ContactItem 
+//                 login="typeofweb2"
+//                 name="Lena"
+//                 department="JavaScript Developer"
+//         />
+//         <ContactItem 
+//                 login="typeofweb1"
+//                 name="Mark"
+//                 department="Java Master"
+//         />
+//         <ContactItem 
+//                 login="typeofweb3"
+//                 name="John"
+//                 department="Head Hunterd"
+//         />
+//       </ul>
+//   );
+// }
+
+
+
+// function ContactItem({login, name, department}) {
+//   const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+//   return (
+//     <li className="item">
+//       <img src= {imgUrl} className="ui mini rounded image" />
+//       <div className="content">
+//         <h4 className="header">{name}</h4>
+//         <div className="description">{department}</div>
+//       </div>
+//     </li>
+//     );  
+// }
+
+/////////////////////Lekcja 6 - klasy jako komponenty w REACT///////////////////////////
+export class AppHeader extends React.Component {
+  render(){
+    return(
     <header className="ui  menu">
       <nav className="ui container">
         <a href="#" className="header item">
@@ -26,10 +85,12 @@ function AppHeader() {
       </nav>
     </header>
     );
+  }
 }
 
-function ContactList() {
-  return (
+export class ContactList extends React.Component {
+  render(){
+    return(
       <ul className="ui relaxed divided list selection">
         <ContactItem 
                 login="typeofweb2"
@@ -47,20 +108,22 @@ function ContactList() {
                 department="Head Hunterd"
         />
       </ul>
-  );
+    )
+  }
 }
 
-function ContactItem({login, name, department}) {
-  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
-  return (
-    <li className="item">
+export class ContactItem extends React.Component {
+  render() {
+    const {login, name, department} = this.props;
+    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+    return (
+      <li className="item">
       <img src= {imgUrl} className="ui mini rounded image" />
       <div className="content">
         <h4 className="header">{name}</h4>
         <div className="description">{department}</div>
       </div>
     </li>
-    );  
+    );
+  }
 }
-
-/////////////////////Lekcja 6 - klasy jako komponenty w REACT///////////////////////////
